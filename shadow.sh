31544db2b58a52ff5f4976e993c98b05cfb3ae7f
ShadowSecurity <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2022-05-18 21:03:39 -0500

    Update shadow.sh

--- shadow.sh
+++ shadow.sh
@@ -1,1 +1,33 @@
 #!/bin/sh
+
+HEIGHT=15
+WIDTH=40
+CHOICE_HEIGHT=4
+BACKTITLE="ShadowSecuritySuite | Shadow-Linux"
+TITLE="Shadow-Linux"
+MENU="Choose one of the following options:"
+
+OPTIONS=(1 "1"
+         2 "2"
+         3 "3")
+
+CHOICE=$(dialog --clear \
+                --backtitle "$BACKTITLE" \
+                --title "$TITLE" \
+                --menu "$MENU" \
+                $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                "${OPTIONS[@]}" \
+                2>&1 >/dev/tty)
+
+clear
+case $CHOICE in
+        1)
+            echo "You chose Option 1"
+            ;;
+        2)
+            echo "You chose Option 2"
+            ;;
+        3)
+            echo "You chose Option 3"
+            ;;
+esac
