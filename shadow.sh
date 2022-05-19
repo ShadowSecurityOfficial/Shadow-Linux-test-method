491a5ee391867324c37405665693243622472ce3
ShadowSecurity <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2022-05-18 23:16:35 -0500

    Update shadow.sh

--- shadow.sh
+++ shadow.sh
@@ -1,1 +1,8 @@
 #!/bin/sh
+
+if [ -d "/bedrock/" ]; then
+    echo "Bedrock Linux detected, continuing the installation." 
+else
+    echo "Bedrock Linux has not been detected as installed, please install Bedrock Linux then rerun the script."
+    exit 1
+fi
