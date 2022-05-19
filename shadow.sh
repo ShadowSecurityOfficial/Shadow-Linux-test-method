8be4859261698d083a5156c856cfdd1b6582bedd
ShadowSecurity <103222783+CY83R-3X71NC710N@users.noreply.github.com>
2022-05-18 23:48:55 -0500

    Update shadow.sh

--- shadow.sh
+++ shadow.sh
@@ -5,9 +5,19 @@ if [[ $EUID -ne 0 ]]; then
    exit 1
 fi
 
+if command -v wget >/dev/null 2>&1 ; then
+    echo "wget found."
+else
+    echo "Missing dependency: wget was not found. Please install."
+    exit 1
+fi
+
 if [ -d "/bedrock/" ]; then
     echo "Bedrock Linux detected, continuing." 
 else
     echo "Bedrock Linux has not been detected as installed, please install Bedrock Linux then rerun the script."
     exit 1
 fi
+
+sudo -E mkdir /shadowtmp/ && cd shadowtmp && sudo -E wget https://kali.download/virtual-images/kali-2022.2/kali-linux-2022.2-vmware-amd64.7z && sudo -E 7za e kali-linux-2022.2-vmware-amd64.7z 
+sudo -E brl import /shadowtmp/kali-linux-2022.2-vmware-amd64.vmdk
